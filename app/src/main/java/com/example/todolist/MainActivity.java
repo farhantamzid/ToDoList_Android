c02ce23a1bc77b1ed851421268e10c4d62f568c6
farhantamzid <farhantamzid@gmail.com>
2024-10-27 03:18:43 +0600

    finished implementing all functionalities

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,18 +1,64 @@
 package com.example.todolist;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView listView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items);
+        listView.setAdapter(itemsAdapter);
+        items.add("First Item");
+        items.add("Second Item");
+        items.add("Third Item");
+        items.add("Fourth Item");
+        items.add("Fifth Item");
+        setupListViewListener();
+
 
 
     }
+
+    private void setupListViewListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Remove the item at the clicked position
+                items.remove(position);
+                // Notify the adapter that the data has changed
+                itemsAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+
+    public void buttonOnClick(View v){
+        EditText etNewItem= (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+    }
+
+
 
 }
